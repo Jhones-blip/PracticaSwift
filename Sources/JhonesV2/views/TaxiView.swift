22cedbdc92e31560517c66fdc8753d7a1525e1c6
Jhones-blip <jhonosoa@gmail.com>
2022-03-15 15:39:44 -0500

    Se finaliza el desarrollo del ejercicio Taxiapp

--- Sources/JhonesV2/views/TaxiView.swift
+++ Sources/JhonesV2/views/TaxiView.swift
@@ -23,24 +23,9 @@ class TaxiView{
         selectTaxiDriver(numberTaxiDriver: selectionTaxiDriver )
 
         //UBICACION DEL TIMER DE VIAJE
+        travelCountDown()
 
-        print("Su viaje a terminado, por favor calififque el conductor!")
-        guard let calificationTaxiDriver = readLine() else {return}
-        guard let rate = Double(calificationTaxiDriver) else { return }
-
-        rateTaxiDriver(numberTaxiDriver: selectionTaxiDriver, rate: rate)
-
-
-        // let rateVerification = 0
-        // if rate<0 || rate > 5 {
-        //     repeat {
-        //         print("Ops! ingresaste una calificacion invalida, intente de nuevo")
-        //         guard let calificationTaxiDriver = readLine() else {return}
-        //         guard let rateVerification = Double(calificationTaxiDriver) else { return }
-        //     } while rateVerification < 0 || rateVerification > 5
-        // } else {
-        //     rateTaxiDriver(numberTaxiDriver: selectionTaxiDriver, rate: rate)
-        // }
+        rateTaxiDriver(numberTaxiDriver: selectionTaxiDriver)
 
         let home = HomeTaxiApp()
         home.homeTaxi()
@@ -57,7 +42,7 @@ class TaxiView{
         var count = 1
         for taxiDriver in taxiDrivers {
     
-            print(count,"- ", taxiDriver.nombre ?? "")
+            print(count,"- ", taxiDriver.nombre ?? "", "- ", taxiDriver.carro ?? "", "- ", "Calificacion " ,taxiDriver.calificacion ?? "", "- " ," Puntaje ",taxiDriver.viajes ?? "")
             count+=1
             
         }
@@ -69,51 +54,50 @@ class TaxiView{
         print("El taxista que selecciono fue \(taxiDrivers[numberTaxiDriver-1].nombre ?? "")")
     }
 
-
-    
-
-    func timer() {
-    
-        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
-
-    }
-
-    @objc func fireTimer() {
-
-        print("Timer fired!")
-    }
-
-    
-
-    func travelCountDown() async { 
+ 
+    func travelCountDown() { 
         let timeTravelRamdom = Int.random(in: 5..<16)
 
-        print("El viaje durara ", timeTravelRamdom, " segundos y terminara en..." )
-
-        for coldownTime in   (0...timeTravelRamdom).reversed(){ 
-            await Task.sleep(nanoseconds: UInt64(1 * Double(timeTravelRamdom)))
-            print(coldownTime)
-        }
+        print("El viaje durara ", timeTravelRamdom, " segundos " )
 
     }
 
     
 
-    func rateTaxiDriver(numberTaxiDriver: Int, rate: Double){
+    func rateTaxiDriver(numberTaxiDriver: Int){
+
+        print("Su viaje a terminado, por favor calififque el conductor!")
+        guard let calificationTaxiDriver = readLine() else {return}
+        guard let rate = Double(calificationTaxiDriver) else { return }
+
+        
+        if rate <= 5 && rate >= 0 {
+
+            let taxiDriver = listTaxiDrivers
+
+            taxiDriver[numberTaxiDriver-1].calificacion = taxiDriver[numberTaxiDriver-1].calificacion + rate
+
+            taxiDriver[numberTaxiDriver-1].viajes = taxiDriver[numberTaxiDriver-1].viajes + 1
+
+            let avgRate = (taxiDriver[numberTaxiDriver-1].calificacion / taxiDriver[numberTaxiDriver-1].viajes)
+
+            print("El promedio actual de calificacion del conductor es: ", avgRate)
+            print("El conductor ha realizado ", taxiDriver[numberTaxiDriver-1].viajes, " viajes")
+            print("")
+            print("Gracias por viajar con nosotro vuelva pronto!")
+
+        }else{
+
+            print("Ops! ingrese una calificacon valida entre 0 y 5")
+            // guard let calificationTaxiDriver = readLine() else {return}
+            // guard let rate = Double(calificationTaxiDriver) else { return }
 
 
-        let taxiDriver = listTaxiDrivers
+            rateTaxiDriver(numberTaxiDriver: numberTaxiDriver)
+        }
+        
 
-        taxiDriver[numberTaxiDriver-1].calificacion = taxiDriver[numberTaxiDriver-1].calificacion + rate
-
-        taxiDriver[numberTaxiDriver-1].viajes = taxiDriver[numberTaxiDriver-1].viajes + 1
-
-        let avgRate = (taxiDriver[numberTaxiDriver-1].calificacion / taxiDriver[numberTaxiDriver-1].viajes)
-
-        print("El promedio actual de calificacion del conductor es: ", avgRate)
-        print("El conductor ha hecho ", taxiDriver[numberTaxiDriver-1].viajes, " viajes")
-        print("")
-        print("Gracias por viajar con nosotro vuelva pronto!")
+        
         
 
     }
@@ -130,4 +114,15 @@ class TaxiView{
 //        await Task.sleep(UInt64(1 * Double(timeTravelRamdom)))
 //        print(coldownTime)
 //    }
+// }
+
+// func timer() {
+    
+//         let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+
+//     }
+
+// @objc func fireTimer() {
+
+//         print("Timer fired!")
 // }
